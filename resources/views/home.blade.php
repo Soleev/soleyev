aadcbb799d9090d6cf8a736e115d73ad0dacd703
Shoxrux <shohruh@yandex.ru>
2025-04-01 17:40:22 +0500

    img to webp title description

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,20 +3,32 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>О себе</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Шохрух Солеев — SEO-оптимизация, разработка сайтов и YouTube SEO для бизнеса</title>
+    <meta name="description" content="Шохрух Солеев — специалист по SEO и веб-разработке, помогающий бизнесам расти в интернете с помощью эффективных стратегий. Он предоставляет услуги по SEO-оптимизации, продвижению на YouTube и созданию сайтов с акцентом на функциональность и качество контента." />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <!-- Google Tag Manager -->
-    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-T69HGSJ');</script>
+    <script>(function (w, d, s, l, i) {
+            w[l] = w[l] || [];
+            w[l].push({
+                'gtm.start':
+                    new Date().getTime(), event: 'gtm.js'
+            });
+            var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
+            j.async = true;
+            j.src =
+                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+            f.parentNode.insertBefore(j, f);
+        })(window, document, 'script', 'dataLayer', 'GTM-T69HGSJ');</script>
     <!-- End Google Tag Manager -->
 </head>
 <body>
 <!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T69HGSJ"
-                  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<noscript>
+    <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T69HGSJ"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe>
+</noscript>
 <!-- End Google Tag Manager (noscript) -->
 
 <div class="container my-5">
@@ -28,7 +40,9 @@
         <div class="col-md-8">
             <h1>Привет, я Шохрух Солеев</h1>
             <p class="lead">Вдохновляю на успех через точные стратегии.</p>
-            <p>Я — специалист по SEO и разработке сайтов. Помогаю бизнесам расти в интернете с помощью правильных стратегий и качественного контента. Понимаю, что каждая деталь имеет значение, и настраиваю сайты так, чтобы они не только привлекали внимание, но и приносили реальную пользу.</p>
+            <p>Я — специалист по SEO и разработке сайтов. Помогаю бизнесам расти в интернете с помощью правильных
+                стратегий и качественного контента. Понимаю, что каждая деталь имеет значение, и настраиваю сайты так,
+                чтобы они не только привлекали внимание, но и приносили реальную пользу.</p>
         </div>
     </div>
 
@@ -46,22 +60,23 @@
             </div>
             <div class="col-md-4">
                 <h3>Разработка сайтов</h3>
-                <p>Создание и поддержка веб-сайтов с фокусом на функциональность, пользовательский опыт и адаптивность.</p>
+                <p>Создание и поддержка веб-сайтов с фокусом на функциональность, пользовательский опыт и
+                    адаптивность.</p>
             </div>
         </div>
     </section>
-{{--
-    <!-- Последние статьи -->
-    <section class="my-5">
-        <h2>Последние статьи из блога</h2>
-        <ul>
-            <li><a href="#">Как улучшить видимость вашего сайта в поисковиках: 5 ключевых шагов</a></li>
-            <li><a href="#">SEO для YouTube: как сделать ваши видео более заметными</a></li>
-            <li><a href="#">Тенденции веб-разработки на 2025 год: что важно знать</a></li>
-        </ul>
-    </section>
+    {{--
+        <!-- Последние статьи -->
+        <section class="my-5">
+            <h2>Последние статьи из блога</h2>
+            <ul>
+                <li><a href="#">Как улучшить видимость вашего сайта в поисковиках: 5 ключевых шагов</a></li>
+                <li><a href="#">SEO для YouTube: как сделать ваши видео более заметными</a></li>
+                <li><a href="#">Тенденции веб-разработки на 2025 год: что важно знать</a></li>
+            </ul>
+        </section>
 
-    --}}
+        --}}
     <!-- Контакты и ссылки на соцсети -->
     <section class="my-5">
         <h2>Контакты</h2>
@@ -77,6 +92,8 @@
     </section>
 </div>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
 </body>
 </html>
